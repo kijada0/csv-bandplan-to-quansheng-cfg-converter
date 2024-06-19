0a5267601bae23fc42096522af24181f094ac737
Kacper Ka <188354e@gmail.com>
2024-06-19 19:12:54 +0200

    update

--- python_src/add_missing_params_to_csv.py
+++ python_src/add_missing_params_to_csv.py
@@ -10,6 +10,34 @@ import config
 def main():
     print("Running add_missing_params_to_csv.py")
 
+    if not os.path.exists(config.band_plan_source_dir):
+        print("Error: band_plan_source_dir does not exist")
+        exit(1)
+
+    band_plan_file_list = os.listdir(config.band_plan_source_dir)
+    for band_plan_file in band_plan_file_list:
+        if band_plan_file.endswith(".csv") and not is_band_plan_exception_file(band_plan_file):
+            add_missing_params_to_csv(band_plan_file)
+
+    print("-" * 40)
+
+
+# -------------------------------------------------------------------------------- #
+
+def is_band_plan_exception_file(band_plan_file):
+    if "FM" in band_plan_file:
+        return True
+
+    return False
+
+
+# -------------------------------------------------------------------------------- #
+
+def add_missing_params_to_csv(band_plan_file):
+    print("-" * 40)
+    print("Processing file: " + band_plan_file)
+
+
 
 # -------------------------------------------------------------------------------- #
 
